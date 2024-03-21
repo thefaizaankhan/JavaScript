15044a504d850be20ef6bb99e0a0fc07199e9621
Faizaan Khan <faizaankhanmfk7701@gmail.com>
2024-03-21 13:04:34 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treats all JS Code as newer version of JS
+
+// alert (3 + 3) // because we are using nodejs, not browser
+
+console.log (5 + 
+    5) // It is not a good practice, Code Readability should be high 
+
+let name = "Faizaan" // string
+let age = 24 // number
+let isLoggedIn = false // bool
+let state // undefined
+let temprature = null // null (empty)
+// number => 2 to power 53 (range)
+// big int (a very large number used in trading, stock markets etc.)
+// string => "" (prefer to use in double quotes)
+// boolean => true/false
+// null => standalone value (representation of empty value)
+// undefined => when value is not assigned
+// symbol => unique (react and figma tools = many components; used in identifying uniqueness of components) 
+
+// object 
+
+console.log(typeof isLoggedIn)
+
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
