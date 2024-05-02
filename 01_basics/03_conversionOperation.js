313d8ba92433d2f1fcb85cedaaa303611d02a94b
Faizaan Khan <faizaankhanmfk7701@gmail.com>
2024-05-02 06:10:17 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = 45
+
+// console.log(typeof score);
+// console.log(typeof(score)); // using a method here
+
+// let valueInNumber = Number(score) // declaring a new variable (valueInNumber) => passing score through 'Number' (Datatypes in capital format; ex.- Number, String, Bool) to convert string into number
+
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber); 
+
+// Don't depend on Numbers in JS
+// "45" => 45 (Number)
+// "45abc" => NaN 
+// true => 1; false => 0
+// null => 0
+// undefined => NaN 
+
+// let isLoggedIn = "faizaan"
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "faizaan" => true
+
+let somenumber = 33.4
+
+let stringNumber = String(somenumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+
+
+
+
+
+
+
