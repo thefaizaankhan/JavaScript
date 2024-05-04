77ab6a5bbace83c85ad87910babaa39b98934138
Faizaan Khan <faizaankhanmfk7701@gmail.com>
2024-05-04 06:48:31 +0000

    comparisons of datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,37 @@
+// console.log(5 < 2);
+// console.log(6 > 2);
+// console.log(2 == 4);
+// console.log(3 != 4);
+
+
+// console.log("4" > 2);
+// console.log("03" > 2);
+
+// When you compare two different values in JavaScript make sure their datatypes are same
+
+// console.log(null > 0);
+// console.log(null != 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// Avoid these conversions
+
+// The reason is that an equality check == and comparisons >, <, >= and <= work differently. Comparisons convert null to a number, treating it as 0. That's why (3) null >= 0 and (1) null > 0 is false.
+
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined < 0);
+
+// Avoid these conversions
+
+// === - Strick Check (It sticktly check values as well as datatypes)
+
+console.log("5" === 5)
+
+// false => bcoz "5" is a string and 5 is number
+
+
+
+
