8313ee55db54c49e7fd6ae6a4975614db9a089a0
Faizaan Khan <faizaankhanmfk7701@gmail.com>
2024-11-28 06:10:38 +0000

    json talk and object de-structure

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,7 +20,8 @@ const regularUser = {
 }
 
 // console.log(regularUser.fullname.userFullName.firstName);
-// when handling data through apis what if fullname object doesn't exist so we use ? (known as Optional Chaining)
+
+// when handling data through apis what if fullname object doesn't exist so we use '?' (known as Optional Chaining)
 // console.log(regularUser.fullname?.userFullName.firstName);
 
 // combining objects
@@ -67,17 +68,53 @@ const dbUsers = [
 // console.log(dbUsers[0].email);
 // console.log(dbUsers[1].email);
 
-console.log(tinderUser);
-// Important** - gives all the keys from an object in the form of array
-console.log(Object.keys(tinderUser));
-// Important** - gives all the value from an object in the form of array
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
-// check if the property email exist
-console.log(tinderUser.hasOwnProperty('email'));
+// console.log(tinderUser);
 
+// Important** - gives all the keys from an object in the form of array
+// console.log(Object.keys(tinderUser));
+
+// Important** - gives all the value from an object in the form of array
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// check if the property email exist
+// console.log(tinderUser.hasOwnProperty('email'));
 
 // put const obj1 = {1: "a", 2: "b"} in browser console and look for different methods which can be applied on objects
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Object De-Structuring
+
+const course = {
+    courseName: "javascript course",
+    price: "1999",
+    courseInstructor: "faizaan"
+}
+
+// console.log(course.courseInstructor);
+
+// Sometimes when accessing values of an object multiple times using . may or may not make the code look messy so to follow a clean code approach we can use a new syntax structure to access the object values
+const {courseName} = course
+const {courseInstructor : instructor} = course // modifying the name
+
+console.log(courseName);
+// console.log(courseInstructor);
+
+// we can also modify the name and the result remain same
+console.log(instructor);
+
+//---------------------------------------------------------
+// Note - A concept of Object De-Structuring comes when working with React.js props -> calling a navbar
+
+// here instead of writing props.company or many other multiple times we do {company} 
+// const navbar = ({company}) => { 
+    
+// }
+
+// navbar(company = "hitesh")
+//---------------------------------------------------------
+
 
 
 
