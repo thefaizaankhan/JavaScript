a3318ab0b3ea9ea36c2fb2e9e02f6d1e1398cdd5
Faizaan Khan <faizaankhanmfk7701@gmail.com>
2024-12-03 13:49:35 +0000

    map method in js

---
+++ 05_iterations/map_method.js
@@ -0,0 +1,18 @@
+// Map()
+
+// Used to create a new array by applying a callback function. It always returns a new array and it does not modify the original array.
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumbers.map((num) => {return num + 100})
+
+// console.log(newNums);
+
+// using foreach loop
+// myNumbers.forEach((num, index) => myNumbers[index] = num + 100)  // OR
+// myNumbers.forEach((num, index, array) => array[index] = num + 100)  // OR
+
+// const newNums = []
+// myNumbers.forEach((num) => newNums.push(num + 100))
+// console.log(newNums);
+
