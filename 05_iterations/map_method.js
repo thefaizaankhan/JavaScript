b355a3185650072c3ac279479df103fbad050376
Faizaan Khan <faizaankhanmfk7701@gmail.com>
2024-12-07 11:49:01 +0000

    added notes

--- 05_iterations/map_method.js
+++ 05_iterations/map_method.js
@@ -16,3 +16,25 @@ const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // myNumbers.forEach((num) => newNums.push(num + 100))
 // console.log(newNums);
 
+//-------------------------------
+
+// Chaining
+
+// yes we can add or chain multiple(2 to 3) map() or filter() to perform an operation on an array.
+
+// In Chaining the value in the first method will be passed to the second method.
+
+const myNewNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNewNumbers
+              .map((num) => num * 10)
+              // here value in the 'num' will never be 1; instead value in the 'num' will be the value passed through the method just before this method -> here in the second statement .map((num) => num + 1) -> num will hold the value of .map((num) => num * 10).
+              // here in the first .map() method we have already done multiplication -> these modified values of the array will be passed to second .map() method and -> +1 will be added to the modified values
+              .map((num) => num + 1)
+              .filter((num) => num >= 40)
+
+// console.log(newNums);
+
+// map() returns the value but you have to return the value explicitly but filter() always return a value 
+
+
