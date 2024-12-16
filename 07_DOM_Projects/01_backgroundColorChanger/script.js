0758173ddafba3f1674650ad82f2f30f8bf24843
Faizaan Khan <faizaankhanmfk7701@gmail.com>
2024-12-16 05:45:27 +0000

    code changes in js and added comments

--- 07_DOM_Projects/01_backgroundColorChanger/script.js
+++ 07_DOM_Projects/01_backgroundColorChanger/script.js
@@ -1,16 +1,19 @@
 const myButtons = document.querySelectorAll('.button')
 // console.log(myButtons);
-const body = document.querySelector('body')
 
-// Events in JS - Every action in the browser -> moving the mouse, button click all are events in Js.
+const body = document.querySelector('body')
+// console.log(body);
+
+// Events in JS - Every action in the browser -> moving the mouse, closing a tab, button click all are events in Js.
 
 myButtons.forEach((button) => {
-  // console.log(button);
+  console.log(button);
   button.addEventListener('click', function(e) {
-    // 'e' is the Event Object
-    // console.log(e);
+    // Refers to the specific element that triggered the click event('.button' that was clicked) and 'e' refers to the Event Object -> we should carry the event object 
+    console.log(e);
     console.log(e.target);
     if(e.target.id === 'grey') {
+      // for better programming practice we can write grey as e.target.id
       body.style.backgroundColor = e.target.id;
     }
     if(e.target.id === 'blue') {
@@ -29,3 +32,34 @@ myButtons.forEach((button) => {
 });
 
 // how we can do the same in another way ? 
+
+// via switch statement 
+
+// const myColor = document.querySelectorAll('.button')
+// const bodyElement = document.querySelector('body')
+
+// myColor.forEach((div) => {
+// console.log(div);
+//   div.addEventListener('click', function(e){
+//     console.log(e);
+//     console.log(e.target);
+//     switch (e.target.id) {
+//       case 'white':
+//         bodyElement.style.backgroundColor = e.target.id
+//         break;
+//       case 'red':
+//         bodyElement.style.backgroundColor = e.target.id
+//         break;
+//       case 'green':
+//         bodyElement.style.backgroundColor = e.target.id
+//         break;
+//       case 'blue':
+//         bodyElement.style.backgroundColor = e.target.id
+//         break;
+//       case 'orange':
+//         bodyElement.style.backgroundColor =  e.target.id
+//       default:
+//         break;
+//     }
+//   })
+// })
